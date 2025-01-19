769a352bf22fb848130ad0c26f71f6a3d99b3f2b
Benjamin Townsend <benjamin@bentownsend.com>
2025-01-19 19:31:26 +0000

    Don't update time if the element doesn't exist

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,14 @@
 // create a function that displays the current date and time
 function displayDateTime() {
+  // check if the element exists before updating it
+  if ($("#date-time").length === 0) {
+    return;
+  }
   var now = new Date();
   var date = now.toLocaleDateString();
   var time = now.toLocaleTimeString();
   var dateTime = date + " " + time;
+
   $("#date-time").text(dateTime);
 }
 
